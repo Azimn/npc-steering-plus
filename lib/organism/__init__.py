bd9b2bc7d5413a920c2de92cca49c473d1fb4b6d
Azimn <jratican@gmail.com>
2026-09-19 22:43:49 -0500

    Expose MLX cognitive backend

--- lib/organism/__init__.py
+++ lib/organism/__init__.py
@@ -2,6 +2,7 @@
 
 from .backend import CognitiveBackend, CognitiveResponse
 from .core import OrganismTurn, PersistentOrganism
+from .mlx_backend import MLXCognitiveBackend
 from .motives import Motive, motives, selected_motive
 from .state import AxisSpec, DEFAULT_AXIS_SPECS, OrganismState
 from .steering import SteeringBinding, SteeringProfile
@@ -11,6 +12,7 @@ __all__ = [
     "CognitiveBackend",
     "CognitiveResponse",
     "DEFAULT_AXIS_SPECS",
+    "MLXCognitiveBackend",
     "Motive",
     "OrganismState",
     "OrganismTurn",
